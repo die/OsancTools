c2b36359cd359e5c6d871e0c6db38bedb18961f3
Luke <58831335+Waifu@users.noreply.github.com>
2022-12-21 09:06:14 -0500

    Resolve conflicts with sniffer branch

--- src/main/java/com/github/waifu/gui/actions/CalculatePlayerExaltationsAction.java
+++ src/main/java/com/github/waifu/gui/actions/CalculatePlayerExaltationsAction.java
@@ -2,74 +2,86 @@ package com.github.waifu.gui.actions;
 
 import com.github.waifu.handlers.RealmeyeRequestHandler;
 import com.github.waifu.util.ExaltCalculator;
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.prefs.Preferences;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 /**
- *
+ * To be documented.
  */
 public class CalculatePlayerExaltationsAction implements ActionListener {
 
-    private JTextField exaltsInput;
-    private JLabel exaltsResult;
+  /**
+   * To be documented.
+   */
+  private final JTextField exaltsInput;
+  /**
+   * To be documented.
+   */
+  private final JLabel exaltsResult;
 
-    /**
-     *
-     * @param exaltsInput
-     * @param exaltsResult
-     */
-    public CalculatePlayerExaltationsAction(JTextField exaltsInput, JLabel exaltsResult) {
-        this.exaltsInput = exaltsInput;
-        this.exaltsResult = exaltsResult;
-    }
+  /**
+   * To be documented.
+   *
+   * @param exaltsInput To be documented.
+   * @param exaltsResult To be documented.
+   */
+  public CalculatePlayerExaltationsAction(final JTextField exaltsInput, final JLabel exaltsResult) {
+    this.exaltsInput = exaltsInput;
+    this.exaltsResult = exaltsResult;
+  }
 
-    /**
-     *
-     * @param e
-     */
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (exaltsInput.getText() != null) {
-            String username = exaltsInput.getText();
-            List<String[]> collection = null;
-            try {
-                if (username != null && !username.equals("") && username.chars().allMatch((Character::isAlphabetic)) && username.length() <= 15) {
-                    collection = RealmeyeRequestHandler.GETExalts(username);
-                    TimeUnit.SECONDS.sleep(1);
-                } else {
-                    exaltsResult.setForeground(Color.yellow);
-                    exaltsResult.setText("Invalid IGN (above 15 characters)");
-                }
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            if (collection != null) {
-                ExaltCalculator calculator = new ExaltCalculator();
-                int stat = calculator.getStat();
-                String statName = switch (stat) {
-                    case 0 -> "Oryx 3";
-                    case 1 -> "Void";
-                    case 2 -> "Shatters";
-                    case 3 -> "MBC";
-                    case 4 -> "Cult";
-                    case 5 -> "Nest";
-                    case 6 -> "Cavern";
-                    case 7 -> "Fungal";
-                    default -> "";
-                };
-                int completes = calculator.calculateCompletions(collection);
-                if (completes >= Preferences.userRoot().getInt("requirement", 100)) {
-                    exaltsResult.setForeground(Color.green);
-                } else {
-                    exaltsResult.setForeground(Color.red);
-                }
-                exaltsResult.setText(username + " has " + calculator.calculateCompletions(collection) + " " + statName + " completes. (>= " + Preferences.userRoot().getInt("requirement", 100) + ")");
-            }
+  /**
+   * To be documented.
+   *
+   * @param e To be documented.
+   */
+  @Override
+  public void actionPerformed(final ActionEvent e) {
+    if (exaltsInput.getText() != null) {
+      final String username = exaltsInput.getText();
+      List<String[]> collection = null;
+      try {
+        if (username != null && !username.equals("") && username.chars().allMatch((Character::isAlphabetic)) && username.length() <= 15) {
+          collection = RealmeyeRequestHandler.GETExalts(username);
+          TimeUnit.SECONDS.sleep(1);
+        } else {
+          exaltsResult.setForeground(Color.yellow);
+          exaltsResult.setText("Invalid IGN (above 15 characters)");
         }
+      } catch (final Exception ex) {
+        ex.printStackTrace();
+      }
+      if (collection != null) {
+        final ExaltCalculator calculator = new ExaltCalculator();
+        final int stat = calculator.getStat();
+        final String statName = switch (stat) {
+          case 0 -> "Oryx 3";
+          case 1 -> "Void";
+          case 2 -> "Shatters";
+          case 3 -> "MBC";
+          case 4 -> "Cult";
+          case 5 -> "Nest";
+          case 6 -> "Cavern";
+          case 7 -> "Fungal";
+          default -> "";
+        };
+        final int completes = calculator.calculateCompletions(collection);
+        if (completes >= Preferences.userRoot().getInt("requirement", 100)) {
+          exaltsResult.setForeground(Color.green);
+        } else {
+          exaltsResult.setForeground(Color.red);
+        }
+        exaltsResult.setText(username + " has " + calculator.calculateCompletions(collection) + " " + statName + " completes. (>= " + Preferences.userRoot().getInt("requirement", 100) + ")");
+      } else {
+        exaltsResult.setForeground(Color.yellow);
+        exaltsResult.setText(username + " has a Private Profile.");
+      }
     }
+  }
 }

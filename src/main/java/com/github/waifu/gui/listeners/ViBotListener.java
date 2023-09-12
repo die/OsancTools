48069dba5f517b10b555a4b93bb572394a8fb9ae
Luke <58831335+Waifu@users.noreply.github.com>
2023-09-11 23:52:47 -0400

    Add ViBotListener.java

---
+++ src/main/java/com/github/waifu/gui/listeners/ViBotListener.java
@@ -0,0 +1,12 @@
+package com.github.waifu.gui.listeners;
+
+import com.github.waifu.entities.ViBotRaider;
+
+public interface ViBotListener {
+  /**
+   * To be documented.
+   *
+   * @param viBotRaider To be documented.
+   */
+  void update(final ViBotRaider viBotRaider);
+}
